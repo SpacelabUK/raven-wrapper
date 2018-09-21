16901a35e274dde245a46ede9855c4b06c874d51
unknown <01secn@gmail.com>
2018-09-21 18:16:01 +0100

    Added log statement to set up to carry out tests

--- raven_wrapper.js
+++ raven_wrapper.js
@@ -6,6 +6,7 @@ module.exports = (ravenWrapperConfig) => {
   let wrapperToHandleUnhandledExceptions;
 
   const setUpRaven = () => {
+    console.log(ravenWrapperConfig.ravenConfig);
     Raven.config(ravenWrapperConfig.ravenConfig).install();
   };
 
